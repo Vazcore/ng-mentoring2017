51c32c6ac73ab3183084f857b71ec5f4f4a97613
Aliaksei Habruseu <Aliaksei.Habruseu@aerlingus.com>
2017-11-27 12:13:44 +0300

    Task 1:  Removed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { CoursesComponent } from './components/courses/courses.component';
-import { CourseComponent } from './components/courses/course/course.component';
-import { NavComponent } from './components/nav/nav.component';
-import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    CoursesComponent,
-    CourseComponent,
-    NavComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
