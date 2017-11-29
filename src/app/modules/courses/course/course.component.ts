1c51a99ba67b1ec880708d6919f2f95aa079ca2e
Aliaksei Habruseu <Aliaksei.Habruseu@aerlingus.com>
2017-11-29 17:54:24 +0300

    Task 2: Added layout for every component.

--- src/app/modules/courses/course/course.component.ts
+++ src/app/modules/courses/course/course.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from './course.model';
+import { Input } from '@angular/core';
+import { Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-course',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
+  @Input() course: Course;
+  @Output() deleteCourse = new EventEmitter<Course>();
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {  }
+
+  onDelete(course: Course) {
+    this.deleteCourse.emit(course);
   }
 
 }
--- src/app/modules/courses/courses.module.ts
+++ src/app/modules/courses/courses.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
+import { CourseService } from './course.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [CoursesComponent, CourseComponent],
-  exports: [CoursesComponent]
+  exports: [CoursesComponent],
+  providers: [
+    CourseService
+  ]
 })
 export class CoursesModule { }
